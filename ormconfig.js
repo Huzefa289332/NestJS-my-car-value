a349b04cd774926305ce7bea71099215f1cb0e2f
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2021-08-28 17:43:43 +0500

    pre-production commit

--- ormconfig.js
+++ ormconfig.js
@@ -1,1 +1,39 @@
-const dbConfig = {};
+const dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.db',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.db',
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true,
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.ts'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
